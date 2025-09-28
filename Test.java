94bdaeaf21b1fdd4c29d35c2b1773488b969bbb6
karezamaulana <karezamaulana@gmail.com>
2025-09-28 17:27:58 +0700

    ubah test java 1

--- Test.java
+++ Test.java
@@ -2,6 +2,7 @@ public class Test{
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama");
         System.out.println("hai hai");
+        System.out.println("halo royan");
     }
 
 
