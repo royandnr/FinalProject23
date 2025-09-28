98866e8e05efc370f7f0f307fa8bb78553935917
Yann <yann@gmail.com>
2025-09-28 17:23:59 +0700

    ubah 2

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test{
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama");
-        
+        System.out.println("hai hai");
     }
 
 
